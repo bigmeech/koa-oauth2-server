cb0624ba550632e7949781aac09e7cc09ace1e69
Larry Eliemenye <larry.eliemenye@tuitravel.com>
2017-04-16 11:09:33 +0100

    Initial commit

---
+++ dist/server.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var _oauth2orizeKoa = require('oauth2orize-koa');
+
+var _oauth2orizeKoa2 = _interopRequireDefault(_oauth2orizeKoa);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
